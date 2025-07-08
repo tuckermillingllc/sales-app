8c196608c602a7580aab54741768f917dd9450e3
Curran Gehring <curran@tuckermilling.com>
2025-07-08 09:21:24 -0500

    Replace default Vue page with Home and Customer views

--- sales-app-frontend/src/services/api.js
+++ sales-app-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1',
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 export default api
--- sales-app-frontend/src/views/Home.vue
+++ sales-app-frontend/src/views/Home.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <CustomerCard v-for="customer in customers" :key="customer.id" :customer="customer" />
+  </div>
 </template>
