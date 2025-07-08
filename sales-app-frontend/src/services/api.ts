64aec1cdb9062fba53ee2d638c4868307238167d
Curran Gehring <curran@tuckermilling.com>
2025-07-08 10:25:04 -0500

    fix api.ts

--- sales-app-frontend/src/services/api.ts
+++ sales-app-frontend/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4173'
 const API = axios.create({
-  baseURL: `${baseURL}/api/v1`
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`
 })
 
 export default API
