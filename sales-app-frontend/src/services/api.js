5a81bed6b3fb4d241d78dce93a98847161a45d7c
Curran Gehring <curran@tuckermilling.com>
2025-07-08 09:44:31 -0500

    fix api.js and Home.vue

--- sales-app-frontend/src/services/api.js
+++ sales-app-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1'
+  baseURL: import.meta.env.VITE_API_URL + '/api/v1/' || 'http://localhost:4173/api/v1'
 })
 
 export default api
