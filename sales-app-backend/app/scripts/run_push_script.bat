7d51e558215738fa23afac12f6753705f504f06f
Curran Gehring <curran@tuckermilling.com>
2025-07-09 14:52:27 -0500

    Add push bat

---
+++ sales-app-backend/app/scripts/run_push_script.bat
@@ -0,0 +1,1 @@
+"C:\Users\Curran\AppData\Local\Programs\Python\Python312\python.exe" "C:\Users\Curran\OneDrive - Tucker Milling LLC\GitHub\Sales-App\sales-app-backend\scripts\push_dealer_categories_to_postgres.py"
