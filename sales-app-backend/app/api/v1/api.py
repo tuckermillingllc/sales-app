3ccb901a0733392af50c478f035aa28c2cb04a5b
Curran Gehring <curran@tuckermilling.com>
2025-07-11 12:49:02 -0500

    Add feed product mapping endpoints

--- sales-app-backend/app/api/v1/api.py
+++ sales-app-backend/app/api/v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from .endpoints import customers, dealer_categories
+from app.api.v1.endpoints import feed_product_mapping
 
 api_router = APIRouter()
 api_router.include_router(customers.router, prefix="/customers", tags=["customers"])
 api_router.include_router(dealer_categories.router, prefix="/dealer-categories", tags=["dealer-categories"])
-api_router.include_router(feed_product_mappings.router, prefix="/feed-product-mappings", tags=["feed-product-mappings"])
+api_router.include_router(feed_product_mapping.router, prefix="/feed-product-mapping", tags=["feed-product-mapping"])
